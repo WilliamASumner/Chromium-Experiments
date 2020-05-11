7e5d46b4f269c3440adb793b1e928b14bf8e1025
William Sumner <williamasumner@gmail.com>
2020-05-11 15:35:29 +0000

    Fix compile.sh for new file arrangement

--- compile.sh
+++ compile.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-### Example
-if [[ "$1" == "all" ]]; then
-		clang++ -Wall -g -fPIC -shared -ldl -o liboriginal.so HelloPrinter.cc # original library example
-		clang++ -Wall -g -fPIC -shared -o intercept.so hello_intercept.cc # interposing library example
-		clang++ -Wall -g main.cc -L. -loriginal -o prog.out # main program example
-fi
-
-#Chrome stuff
+#Chrome script
 clang++ -shared -fPIC -ldl -o intercept-chrome.so chrome_intercept.cc 
