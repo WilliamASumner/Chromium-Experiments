e1ccb2e52e9dc19b8da2e4ccc0e11dc5c093937d
William Sumner <williamasumner@gmail.com>
2020-05-20 16:50:42 +0000

    Fix script for use with Makefile

--- permutate.sh
+++ permutate.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 FILE_PREFIX="output"
-ITERATIONS=10
+ITERATIONS=1
+COMMAND="echo"
+FLAGS=""
 
 declare -a configs governors iterconfig itergovernor sites # declare arrays
 #configs=("4l-0b" "4l-4b" "0l-4b" "4l-1b" "4l-2b" "2l-0b" "1l-0b" "0l-1b" "0l-2b") # all core configs to test with
@@ -52,21 +54,23 @@ gen_itergovernor() { # generate a permutation of the governors
 
 
 echo_usage() {
-    echo "usage: $0 [-h | -f PREFIX | -i ITERATIONS | -c COMMAND]"
+    echo "usage: $0 [-h | -p PREFIX | -i ITERATIONS | -c COMMAND | -f FLAGS]"
     echo "-h : Help,       shows this usage message"
-    echo "-f : Prefix,     the prefix to use for files generated from the script. Useful for labelling experiment trials"
+    echo "-p : Prefix,     the prefix to use for files generated from the script. Useful for labelling experiment trials"
     echo "-i : Iterations, number of iterations to run"
     echo "-c : Command,    command to be run on each permutation"
+    echo "-f : Flags,      flags to be run with the given command"
+    echo "Predefined values: #site = website, #gov = freq gov, #iter = iteration, #config = core configuration"
     exit 0
 }
 
-while getopts ":hi:f:c:" opt; do
+while getopts ":hp:i:c:f:" opt; do
     case $opt in
         h)
             echo_usage
             ;;
 
-        f)
+        p)
             FILE_PREFIX=$OPTARG
             ;;
 
@@ -76,6 +80,10 @@ while getopts ":hi:f:c:" opt; do
         c)
             COMMAND=$OPTARG
             ;;
+
+        f)
+            FLAGS=$OPTARG
+            ;;
         \?) # invalid arg
             echo "Invalid arg -$OPTARG" >&2
             echo_usage
@@ -83,8 +91,8 @@ while getopts ":hi:f:c:" opt; do
 
         :) # missing arg
             case $OPTARG in
-                f)
-                    echo "$0: missing prefix for option '-f'" >&2
+                p)
+                    echo "$0: missing prefix for option '-p'" >&2
                     echo_usage
                     ;;
                 i)
@@ -93,6 +101,10 @@ while getopts ":hi:f:c:" opt; do
                     ;;
                 c)
                     echo "$0: missing command argument for option '-c'"
+                    echo_usage
+                    ;;
+                f)
+                    echo "$0: missing flags argument for option '-f'"
                     echo_usage
                     ;;
             esac
@@ -116,9 +128,14 @@ for (( iter=1; iter <=$ITERATIONS; iter++ )); do
                 ID=`mktemp -u XXXXXXXX` # unique experiment id
                 export CORE_CONFIG=$config
                 export LOG_FILE="$FILE_PREFIX-$ID"
-                echo "on permutation $config $FILE_PREFIX-$ID $site"
+                FLAGS=${FLAGS//'#config'/"$config"}
+                FLAGS=${FLAGS//'#site'/"$site"}
+                FLAGS=${FLAGS//'#iter'/"$iter"}
+                FLAGS=${FLAGS//'#gov'/"$gov"}
+                echo "on permutation CORE_CONFIG=$config LOG_FILE=$FILE_PREFIX-$ID"
+                echo "'$COMMAND' $FLAGS"
                 #export GOVERNOR=$gov # not needed right now
-                $COMMAND
+                $COMMAND "$FLAGS"
                 #./run.sh $config $FILE_PREFIX $site
                 RETVAL=$?
                 if [[ "$RETVAL" == "1" ]]; then # script didn't like something...
