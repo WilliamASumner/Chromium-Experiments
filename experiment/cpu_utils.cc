41b48c0932a9036e077234391b7c11880c322526
William Sumner <williamasumner@gmail.com>
2020-05-20 15:31:30 +0000

    Rearrange cpu_utils

---
+++ experiment/cpu_utils.cc
@@ -0,0 +1,69 @@
+//Written by Will Sumner
+//Under Dr. Vinicius Petrucci
+#include "cpu_utils.hh"
+#include <random> // better than rand()
+#include <sched.h>
+
+
+static std::random_device rd;
+static std::mt19937 mt(rd());
+static std::uniform_int_distribution<int> dist(0,3);
+
+void _init_affinity() {
+    return;
+}
+
+cpu_set_t _set_affinity_little(void) {
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+    SET_AFF_LITTLE
+    return mask;
+}
+
+cpu_set_t _set_affinity_big(void) {
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+    SET_AFF_BIG
+    return mask;
+}
+
+cpu_set_t _set_affinity_all(void) {
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+    SET_AFF_ALL
+    return mask;
+}
+
+cpu_set_t _set_affinity_permute(int bigs, int lils) {
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+    CPU_SET(0,&mask);
+    CPU_SET(1,&mask);
+    CPU_SET(2,&mask);
+    CPU_SET(3,&mask);
+    CPU_SET(4,&mask);
+    CPU_SET(5,&mask);
+    CPU_SET(6,&mask);
+    CPU_SET(7,&mask);
+
+    int choice;
+
+    while (bigs > 0) {
+        choice  = dist(mt) + 4;
+        if (!CPU_ISSET(choice,&mask)) {
+            continue;
+        }
+        CPU_CLR(choice,&mask);
+        bigs--;
+    }
+
+    while (lils > 0) {
+        choice  = dist(mt);
+        if (!CPU_ISSET(choice,&mask)) {
+            continue;
+        }
+        CPU_CLR(choice,&mask);
+        bigs--;
+    }
+    return mask;
+}
---
+++ experiment/cpu_utils.hh
@@ -0,0 +1,49 @@
+//Written by Will Sumner
+//Under Dr. Vinicius Petrucci
+/* Macro to allow affinity changes within a scope, only needs to be used once */
+
+#ifndef CPU_UTILS_H
+#define CPU_UTILS_H
+#include <sched.h>
+
+/* Macro to set affinity to all LITTLE cores */
+#define SET_AFF_LITTLE    \
+    CPU_ZERO(&mask);  \
+    CPU_SET(0,&mask); \
+    CPU_SET(1,&mask); \
+    CPU_SET(2,&mask); \
+    CPU_SET(3,&mask); \
+    sched_setaffinity(0,sizeof(mask),&mask);
+
+/* Macro to set affinity to all big cores */
+#define SET_AFF_BIG       \
+    CPU_ZERO(&mask);  \
+    CPU_SET(4,&mask); \
+    CPU_SET(5,&mask); \
+    CPU_SET(6,&mask); \
+    CPU_SET(7,&mask); \
+    sched_setaffinity(0,sizeof(mask),&mask);
+
+/* Macro to set affinity to all cores */
+#define SET_AFF_ALL       \
+    CPU_ZERO(&mask);  \
+    CPU_SET(0,&mask); \
+    CPU_SET(1,&mask); \
+    CPU_SET(2,&mask); \
+    CPU_SET(3,&mask); \
+    CPU_SET(4,&mask); \
+    CPU_SET(5,&mask); \
+    CPU_SET(6,&mask); \
+    CPU_SET(7,&mask); \
+    sched_setaffinity(0,sizeof(mask),&mask);
+
+void _init_affinity();
+
+cpu_set_t _set_affinity_little(void);
+
+cpu_set_t _set_affinity_big(void);
+
+cpu_set_t _set_affinity_all(void);
+
+cpu_set_t _set_affinity_permute(int bigs, int lils);
+#endif
