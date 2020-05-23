7ed19018056965a821109b74803469e6bf353bd1
William Sumner <williamasumner@gmail.com>
2020-05-23 17:22:12 +0000

    Add data summary to clean-data script

--- clean-data.sh
+++ clean-data.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-find logs/ -size 0 | xargs rm # remove empty entries, sometimes it quits before they can log
+
+FUNC_LOG="func_latencies.log"
+SUMMARY_LOG="summary.log"
+
+find logs/ -size 0 | xargs rm || true # remove empty entries, some processes quit before they can log
+cd logs
+echo -n "" > $FUNC_LOG
+
+for file in `ls -1`; do
+    if [[ "$file" == "format.md" ]]; then
+        continue
+    fi
+    # Generate function latency list
+    cat $file | tail +4 | awk '{ print $7"\t"$9 }' >> $FUNC_LOG
+done
+
+cat $FUNC_LOG | sort | uniq > $SUMMARY_LOG
