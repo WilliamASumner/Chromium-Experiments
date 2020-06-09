58133aafae2425b8ff23843978b8e106724bd9f6
Will Sumner <williamasumner@gmail.com>
2020-06-09 19:37:32 -0400

    Make summarize.sh directory independent

--- summarize.sh
+++ summarize.sh
@@ -2,11 +2,13 @@
 
 FUNC_LOG="func_latencies.log"
 SUMMARY_LOG="summary.log"
-EMPTY_FILES=`find logs/ -size 0`
+LOG_DIR="$PWD/logs/"
+EMPTY_FILES=`find $LOG_DIR -size 0`
 if [[ "$EMPTY_FILES" != "" ]]; then
-    find logs/ -size 0 | xargs rm || true # remove empty entries, some processes quit before they can log
+    find $LOG_DIR -size 0 | xargs rm || true # remove empty entries, some processes quit before they can log
 fi
-cd logs
+
+cd $LOG_DIR
 echo -n "" > $FUNC_LOG
 
 for file in `ls -1`; do
