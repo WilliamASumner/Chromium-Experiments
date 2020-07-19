76ab4d4c27d0d002c7f53fa3c1e78d7f8d72e9e3
William Sumner <williamasumner@gmail.com>
2020-07-19 15:42:55 +0000

    Remove macros and fix waiting bug

--- experiment/cpu_utils.hh
+++ experiment/cpu_utils.hh
@@ -4,38 +4,6 @@
 
 #ifndef CPU_UTILS_H
 #define CPU_UTILS_H
-#include <sched.h>
-
-/* Macro to set affinity to all LITTLE cores */
-#define SET_AFF_LITTLE    \
-    CPU_ZERO(&mask);  \
-    CPU_SET(0,&mask); \
-    CPU_SET(1,&mask); \
-    CPU_SET(2,&mask); \
-    CPU_SET(3,&mask); \
-    sched_setaffinity(0,sizeof(mask),&mask);
-
-/* Macro to set affinity to all big cores */
-#define SET_AFF_BIG       \
-    CPU_ZERO(&mask);  \
-    CPU_SET(4,&mask); \
-    CPU_SET(5,&mask); \
-    CPU_SET(6,&mask); \
-    CPU_SET(7,&mask); \
-    sched_setaffinity(0,sizeof(mask),&mask);
-
-/* Macro to set affinity to all cores */
-#define SET_AFF_ALL       \
-    CPU_ZERO(&mask);  \
-    CPU_SET(0,&mask); \
-    CPU_SET(1,&mask); \
-    CPU_SET(2,&mask); \
-    CPU_SET(3,&mask); \
-    CPU_SET(4,&mask); \
-    CPU_SET(5,&mask); \
-    CPU_SET(6,&mask); \
-    CPU_SET(7,&mask); \
-    sched_setaffinity(0,sizeof(mask),&mask);
 
 void _init_affinity();
 
@@ -45,9 +13,10 @@ cpu_set_t set_affinity_big(void);
 
 cpu_set_t set_affinity_all(void);
 
-cpu_set_t get_affinity_permute(int bigs, int lils);
+cpu_set_t get_affinity_permute(int bigs, int lils, std::mt19937 mt);
 
 void set_affinity_with_mask(cpu_set_t mask);
 
 unsigned int get_curr_cpu();
+
 #endif
