37a65ec46fa7c9dc4244fe2de1b6de051c900c16
William Sumner <williamasumner@gmail.com>
2020-07-19 15:43:17 +0000

    Remove macros and remove waiting bug

--- experiment/cpu_utils.cc
+++ experiment/cpu_utils.cc
@@ -11,27 +11,25 @@ void _init_affinity() {
 cpu_set_t set_affinity_little(void) {
     cpu_set_t mask;
     CPU_ZERO(&mask);
-    SET_AFF_LITTLE
+    CPU_SET(0,&mask);
+    CPU_SET(1,&mask);
+    CPU_SET(2,&mask);
+    CPU_SET(3,&mask);
+    sched_setaffinity(0,sizeof(mask),&mask);
     return mask;
 }
 
 cpu_set_t set_affinity_big(void) {
     cpu_set_t mask;
     CPU_ZERO(&mask);
-    SET_AFF_BIG
+    CPU_SET(4,&mask);
+    CPU_SET(5,&mask);
+    CPU_SET(6,&mask);
+    CPU_SET(7,&mask);
     return mask;
 }
 
 cpu_set_t set_affinity_all(void) {
-    cpu_set_t mask;
-    CPU_ZERO(&mask);
-    SET_AFF_ALL
-    return mask;
-}
-
-cpu_set_t gen_affinity_permute(int bigs, int lils, std::mt19937 mt) {
-    static std::uniform_int_distribution<int> dist(0,3);
-
     cpu_set_t mask;
     CPU_ZERO(&mask);
     CPU_SET(0,&mask);
@@ -42,25 +40,32 @@ cpu_set_t gen_affinity_permute(int bigs, int lils, std::mt19937 mt) {
     CPU_SET(5,&mask);
     CPU_SET(6,&mask);
     CPU_SET(7,&mask);
+    return mask;
+}
+
+cpu_set_t get_affinity_permute(int bigs, int lils, std::mt19937 mt) {
+    static std::uniform_int_distribution<int> dist(0,3);
 
     int choice;
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
 
     while (bigs > 0) {
         choice  = dist(mt) + 4;
-        if (!CPU_ISSET(choice,&mask)) {
+        if (CPU_ISSET(choice,&mask)) {
             continue;
         }
-        CPU_CLR(choice,&mask);
+        CPU_SET(choice,&mask);
         bigs--;
     }
 
     while (lils > 0) {
         choice  = dist(mt);
-        if (!CPU_ISSET(choice,&mask)) {
+        if (CPU_ISSET(choice,&mask)) {
             continue;
         }
-        CPU_CLR(choice,&mask);
-        bigs--;
+        CPU_SET(choice,&mask);
+        lils--;
     }
     return mask;
 }
