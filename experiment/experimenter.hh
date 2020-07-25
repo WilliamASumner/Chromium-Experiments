b15053aff1d528101b0854e80c3c366cbf9ef95c
William Sumner <williamasumner@gmail.com>
2020-07-25 13:16:45 +0000

    Fix missing symbol with fentry/fexit

--- experiment/experimenter.hh
+++ experiment/experimenter.hh
@@ -1,3 +1,5 @@
+#include <string>
+
 void experiment_start_timer();
 
 void experiment_init(const char*);
@@ -8,6 +10,6 @@ void experiment_mark_page_start();
 
 void experiment_mark_page_loaded();
 
-void experiment_fentry(const char* func_name);
+void experiment_fentry(std::string func_name);
 
-void experiment_fexit(const char* func_name);
+void experiment_fexit(std::string func_name);
